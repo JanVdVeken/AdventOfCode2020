f98905c2e116eaee74e2892fa4a05aeaf3f10311
Jan Van der Veken <vekie1@hotmail.com>
2020-12-01 23:30:52 +0100

    Plat fixed the weird shizzle

---
+++ AdventOfCode2020/Program.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCode2020.Days;
+using AdventOfCode2020.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        public static List<Day> days = new()
+        {
+            new Day01(),
+            new Day02(),
+        };
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Which Day do you want ?");
+                days.Where(x => x.Title != null).ToList().ForEach(x => x.PrintInfo());
+                var selectedDay = days.First(x => x.DayNumber == Convert.ToInt32(Console.ReadLine()));
+                //Execute the correct puzzle
+                selectedDay.HandleSelect();
+                //Go back to the overview menu
+                selectedDay.Deselect();
+            }
+        }
+    }
+}
