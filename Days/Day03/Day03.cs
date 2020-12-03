d2b997d877e8e91eb2972ba5aa5b78824a1f3d27
PROTOPIA\jan.vanderveken <jan.vanderveken@protime.eu>
2020-12-03 08:43:33 +0100

    Finished D2P1 and D2P2

--- Days/Day03/Day03.cs
+++ Days/Day03/Day03.cs
@@ -1,4 +1,5 @@
 ﻿using AdventOfCode2020.Shared;
+using Days.Day03;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -8,30 +9,47 @@ namespace AdventOfCode2020.Days
 {
     public class Day03 : Day
     {
-        public List<string> input;
+        public string[] input;
+        private Toboggan toboggan;
+        //input[y][x]
+        
         public Day03()
         {
             DayNumber = 3;
-            Title = "TBD";
+            Title = "Toboggan Trajectory";
+            
         }
 
         public override void Puzzle1()
         {
-            var temp = 0;
+            toboggan = new Toboggan(0, 0, 3, 1);
+            int hits = toboggan.Travel(input);
 
-            Console.WriteLine(temp);
+            Console.WriteLine($"We have hit {hits} trees");
         }
 
         public override void Puzzle2()
         {
-            var temp = 0;
- 
-            Console.WriteLine(temp);
+            int hits = 1;
+            List<Toboggan> toboggans = new List<Toboggan>()
+            {
+                new Toboggan(0, 0, 1, 1),
+                new Toboggan(0, 0, 3, 1),
+                new Toboggan(0, 0, 5, 1),
+                new Toboggan(0, 0, 7, 1),
+                new Toboggan(0, 0, 1, 2),
+            };
+            foreach(Toboggan toboggan in toboggans)
+            {
+                hits *= toboggan.Travel(input);
+            }
+            
+            Console.WriteLine($"We have hit {hits} trees");
         }
 
         public override void GatherInput()
         {
-            input = ReadFile().ToList();
+            input = ReadFile().ToArray();
         }
     }
 }
---
+++ Days/Day03/Toboggan.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Days.Day03
+{
+    class Toboggan
+    {
+        public int xPos;
+        public int yPos;
+        public int xIncrement;
+        public int yIncrement;
+        
+
+        public Toboggan(int x, int y, int incX, int incY)
+        {
+            xPos = x;
+            yPos = y;
+            xIncrement = incX;
+            yIncrement = incY;
+
+        }
+        public int Travel(string[] map)
+        {
+            int treeHits = 0;
+            int width = map[0].Length; //Lengte van de eerste rij
+            int lenght = map.Length; //Lengte van de eerste kolom
+            while(yPos < lenght)
+            {
+                if(map[yPos][xPos] == '#')
+                {
+                    treeHits += 1;
+                    //Console.WriteLine($"Hit on xPos = {xPos}, yPos = {yPos}");
+                }
+
+                yPos += yIncrement;
+                xPos += xIncrement;
+                
+                if(xPos > width -1)
+                {
+                    xPos = xPos - width;
+                }
+                //Console.WriteLine($"xPos = {xPos}, yPos = {yPos}");
+            }
+            return treeHits;
+        }
+    }
+}
