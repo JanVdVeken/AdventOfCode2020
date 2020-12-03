f5ceb6c3bc97038a62693f7c476df05c84caf326
Jan Van der Veken <vekie1@hotmail.com>
2020-12-03 08:12:46 +0100

    Day3

---
+++ Days/Day03/Day03.cs
@@ -0,0 +1,38 @@
+﻿using AdventOfCode2020.Shared;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020.Days
+{
+    public class Day03 : Day
+    {
+        public List<string> input;
+        public Day03()
+        {
+            DayNumber = 3;
+            Title = "TBD";
+        }
+
+        public override void Puzzle1()
+        {
+            var temp = 0;
+
+            Console.WriteLine(temp);
+        }
+
+        public override void Puzzle2()
+        {
+            var temp = 0;
+ 
+            Console.WriteLine(temp);
+        }
+
+        public override void GatherInput()
+        {
+            input = ReadFile().ToList();
+        }
+    }
+}
+
