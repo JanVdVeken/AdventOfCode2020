fde6ae9537633010985e3aa0549171b459cdbf7b
Jan Van der Veken <vekie1@hotmail.com>
2020-12-03 17:40:52 +0100

    Ready for day 4

---
+++ Days/Day04/Day04.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode2020.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Days
+{
+    public class Day04 : Day
+    {
+        public List<int> input;
+        public Day04()
+        {
+            DayNumber = 4;
+            Title = "TBD";
+        }
+
+        public override void Puzzle1()
+        {
+            
+        }
+
+        public override void Puzzle2()
+        {
+            
+        }
+
+        public override void GatherInput()
+        {
+            input = ReadFile().Select(int.Parse).ToList();
+        }
+    }
+}
