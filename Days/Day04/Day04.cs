4668bd95260e85e640a880fd696561f9e37233ec
PROTOPIA\jan.vanderveken <jan.vanderveken@protime.eu>
2020-12-04 09:08:34 +0100

    Day 4 is not yet solved

--- Days/Day04/Day04.cs
+++ Days/Day04/Day04.cs
@@ -1,22 +1,38 @@
 ﻿using AdventOfCode2020.Shared;
+using Days.Day04;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace AdventOfCode2020.Days
 {
     public class Day04 : Day
     {
-        public List<int> input;
+        private List<Passport> input;
         public Day04()
         {
             DayNumber = 4;
-            Title = "TBD";
+            Title = "Passport Processing";
+            input = new List<Passport>();
         }
 
         public override void Puzzle1()
         {
-            
+            List<string> needsToExist = new List<string>()
+            {
+                "byr",
+                "iyr",
+                "eyr",
+                "hgt",
+                "hcl",
+                "ecl",
+                "pid",
+                //"cid",
+            };
+            var validPassports = input.Count(x => x.CheckIfKeysExists(needsToExist));
+            Console.WriteLine($"We got {validPassports} valid passports.");
+
         }
 
         public override void Puzzle2()
@@ -26,7 +42,32 @@ namespace AdventOfCode2020.Days
 
         public override void GatherInput()
         {
-            input = ReadFile().Select(int.Parse).ToList();
+            var lines = ReadFile().ToList();
+            var formattedInputs = new List<string>();
+            StringBuilder sb = new StringBuilder();
+            foreach(string line in lines)
+            {
+                if (!string.Equals(line, string.Empty))
+                {
+                    sb.Append(line.Trim() + " ");
+                }
+                else
+                {
+                    formattedInputs.Add(sb.ToString());
+                    sb.Clear();
+                }
+                if (line == lines.Last())
+                {
+                    formattedInputs.Add(sb.ToString());
+                    sb.Clear();
+                }
+            }
+
+            foreach (string formattedLine in formattedInputs)
+            {
+                Console.WriteLine(formattedLine.Trim());
+                input.Add(new Passport(formattedLine.Trim()));
+            }
         }
     }
 }
---
+++ Days/Day04/Passport.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Days.Day04
+{
+    
+    class Passport
+    {
+        public Dictionary<string, string> fields = new Dictionary<string, string>();
+
+        public Passport(string inputString)
+        {
+            List<string> inputPairs = inputString.Split(" ").ToList();
+            foreach(string inputPair in inputPairs)
+            {
+                var split = inputPair.Split(":");
+                var key = split[0];
+                var value = split[1];
+
+                fields.Add(key, value);
+
+            }
+        }
+
+        public void PrintPassport()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("This password has the following pairs: " + System.Environment.NewLine);
+            foreach(string key in fields.Keys)
+            {
+                sb.Append($" \t key: {key} => value: {fields[key]} \n");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+
+        public bool CheckIfKeysExists(List<string> keyToCheck)
+        {
+            bool ans = true;
+            foreach(string key in keyToCheck)
+            {
+                ans = fields.ContainsKey(key) && ans;
+            }
+            return ans;
+        }
+    }
+}
