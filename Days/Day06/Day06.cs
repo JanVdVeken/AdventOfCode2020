d949b54e0227dbfc73fe7922c30630f369ce5648
Jan Van der Veken <vekie1@hotmail.com>
2020-12-06 19:01:05 +0100

    Fixed D6P1

--- Days/Day06/Day06.cs
+++ Days/Day06/Day06.cs
@@ -1,4 +1,5 @@
 ﻿using AdventOfCode2020.Shared;
+using Days.Day06;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ namespace AdventOfCode2020.Days
 {
     public class Day06 : Day
     {
-        public List<string> input;
+        private List<GroupOfAnswers> input = new List<GroupOfAnswers>();
         public Day06()
         {
             DayNumber = 6;
@@ -17,7 +18,14 @@ namespace AdventOfCode2020.Days
 
         public override void Puzzle1()
         {
+            int amount = 0;
+            foreach(GroupOfAnswers group in input)
+            {
+                amount += group.returnKeySize();
+                Console.WriteLine(group.answers.Keys.Count());
+            }
 
+            Console.WriteLine($"We've got {amount} yes answers");
         }
 
         public override void Puzzle2()
@@ -34,7 +42,7 @@ namespace AdventOfCode2020.Days
             {
                 if (!string.Equals(line, string.Empty))
                 {
-                    sb.Append(line.Trim() + " ");
+                    sb.Append(line.Trim());
                 }
                 else
                 {
@@ -42,15 +50,16 @@ namespace AdventOfCode2020.Days
                     sb.Clear();
                 }
             }
+            
             formattedInputs.Add(sb.ToString());
-            sb.Clear();
+            sb.Clear();         
 
             foreach (string formattedLine in formattedInputs.Where(x => x.Trim() != string.Empty))
             {
-                Console.WriteLine(formattedLine.Trim());
-                //input.Add(new Passport(formattedLine.Trim()));
+                input.Add(new GroupOfAnswers(formattedLine.Trim()));
+                //Console.WriteLine(formattedLine);
             }
         }
-    }
+
     }
 }
---
+++ Days/Day06/GroupOfAnswers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Days.Day06
+{
+    class GroupOfAnswers
+    {
+        public Dictionary<char, int> answers = new Dictionary<char, int>();
+
+        public GroupOfAnswers(string input)
+        {
+            foreach (char character in input)
+            {
+                if (answers.ContainsKey(character))
+                {
+                    answers[character] += 1;
+                }
+                else
+                {
+                    answers.Add(character,1);
+                }
+            }
+        }
+
+        public int returnKeySize()
+        {
+            //Console.WriteLine($"Group with total: {answers.Keys.Count}");
+            return answers.Keys.Count();
+        }
+    }
+}
