1e15836bef378cca842d011ab356d037a2583270
Jan Van der Veken <vekie1@hotmail.com>
2020-12-09 22:54:13 +0100

    Added Day10 via template

---
+++ Days/Day11/Day11.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCode2020.Shared;
+using Days.Day11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020.Days
+{
+    public class Day11 : Day
+    {
+        private List<string> input = new List<string>();
+
+        public Day11()
+        {
+            DayNumber = 0;
+            Title = "TBD";
+        }
+
+        public override void Puzzle1()
+        {
+
+        }
+
+        public override void Puzzle2()
+        {
+
+
+        }
+
+        public override void GatherInput()
+        {
+            input = ReadFile().ToList();
+        }
+    }
+}
