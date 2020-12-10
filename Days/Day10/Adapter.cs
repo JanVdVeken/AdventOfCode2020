17a959125dfff74bf654530baba75673902c9b66
PROTOPIA\jan.vanderveken <jan.vanderveken@protime.eu>
2020-12-10 08:56:40 +0100

    No solution just yet for D10P1

---
+++ Days/Day10/Adapter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Days.Day10
+{
+    class Adapter
+    {
+        private int minReceivingJolts;
+        public int ownJolts;
+        public int sendingJolts;
+        public Adapter(int current, int max)
+        {
+            ownJolts = current;
+            minReceivingJolts = ownJolts - 3;
+            sendingJolts = max;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Adapter with following specs: {minReceivingJolts} receiving, {ownJolts} own rating, {sendingJolts} sending. {rangeOfReceivingJolts()[0]} en {rangeOfReceivingJolts()[1]}");
+        }
+
+        public int[] rangeOfReceivingJolts()
+        {
+            int[] returnArray = new int[2] { minReceivingJolts, ownJolts-1};
+            return returnArray;
+        }
+    }
+}
--- Days/Day10/Day10.cs
+++ Days/Day10/Day10.cs
@@ -9,17 +9,45 @@ namespace AdventOfCode2020.Days
 {
     public class Day10 : Day
     {
-        private List<string> input = new List<string>();
+        private List<Adapter> inputs = new List<Adapter>();
 
         public Day10()
         {
-            DayNumber = 0;
-            Title = "TBD";
+            DayNumber = 10;
+            Title = "Adapter Array";
         }
 
         public override void Puzzle1()
         {
-            Console.WriteLine(input.First());   
+            inputs.ForEach(x => x.PrintInfo());
+            List<Adapter> checkedList = new List<Adapter>();
+            Dictionary<int, int> countSteps = new Dictionary<int, int>();
+            int currentJolts = 0;
+
+            while (checkedList.Count() != inputs.Count())
+            {
+                Console.WriteLine($"{inputs.First().rangeOfReceivingJolts()[0]} {inputs.First().rangeOfReceivingJolts()[1]}");
+                int newJolts = inputs.Where(x => Enumerable.Range(x.rangeOfReceivingJolts()[0], x.rangeOfReceivingJolts()[1]).Contains(currentJolts)).ToList().Min(y => y.ownJolts);
+                
+                if (countSteps.ContainsKey(newJolts-currentJolts)) 
+                {
+                    countSteps[newJolts - currentJolts] += 1; 
+                }
+                else
+                {
+                    countSteps[newJolts - currentJolts] = 1;
+                }
+                checkedList.Add(inputs.Single(x => x.ownJolts == newJolts));
+                currentJolts = newJolts;
+            }
+
+            foreach(int key in countSteps.Keys)
+            {
+                Console.WriteLine($"{key} => {countSteps[key]}");
+            }
+            
+
+            
         }
 
         public override void Puzzle2()
@@ -30,7 +58,13 @@ namespace AdventOfCode2020.Days
 
         public override void GatherInput()
         {
-            input = ReadFile().ToList();
+            var inputListInt = ReadFile().Select(int.Parse).ToList();
+            int max = inputListInt.Max() + 3;
+            foreach(int element in inputListInt)
+            {
+                inputs.Add(new Adapter(element, max));
+            }
+            
         }
     }
 }
