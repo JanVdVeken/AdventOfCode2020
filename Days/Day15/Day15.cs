92b507bf69561bbf863ab006ec24c673552f49e8
PROTOPIA\jan.vanderveken <jan.vanderveken@protime.eu>
2020-12-15 12:28:31 +0100

    Solved D15P2

--- Days/Day15/Day15.cs
+++ Days/Day15/Day15.cs
@@ -19,33 +19,56 @@ namespace AdventOfCode2020.Days
 
         public override void Puzzle1()
         {
+            int goal = 2020;
+            Dictionary<int, int> input1 = input;
             //input.Keys.ToList().ForEach(x => Console.WriteLine($"Number: {x}, last seen: {input[x]}"));
-            int currentCounter = input.Count()+1;
-            int previousNumber = input.Keys.Last();
-            input.Remove(input.Keys.Last());
-            while (currentCounter <= 2020)
+            int currentCounter = input1.Count()+1;
+            int previousNumber = input1.Keys.Last();
+            input.Remove(input1.Keys.Last());
+            while (currentCounter <= goal)
             {
-                int newNumber = Memory.GetNewNumber(previousNumber, currentCounter, input);
+                int newNumber = Memory.GetNewNumber(previousNumber, currentCounter, input1);
                 //Console.WriteLine($"PreviousNumber = {previousNumber} => newNumber = {newNumber}");
-                if(input.ContainsKey(previousNumber))
+                if(input1.ContainsKey(previousNumber))
                 {
-                    input[previousNumber] = currentCounter-1;
+                    input1[previousNumber] = currentCounter-1;
                 }
                 else
                 {
-                    input.Add(previousNumber, currentCounter-1);
+                    input1.Add(previousNumber, currentCounter-1);
                 }
                 previousNumber = newNumber;
                 currentCounter++;
             }
             //input.Keys.ToList().ForEach(x => Console.WriteLine($"Number: {x}, last seen: {input[x]}"));
-            Console.WriteLine($"The number on position 2020 = {previousNumber}");
+            Console.WriteLine($"The number on position {goal} = {previousNumber}");
         }
 
         public override void Puzzle2()
         {
-
-
+            int goal = 30000000;
+            Dictionary<int, int> input2 = input;
+            //input.Keys.ToList().ForEach(x => Console.WriteLine($"Number: {x}, last seen: {input[x]}"));
+            int currentCounter = input2.Count() + 1;
+            int previousNumber = input2.Keys.Last();
+            input2.Remove(previousNumber);
+            while (currentCounter <= goal)
+            {
+                int newNumber = Memory.GetNewNumber(previousNumber, currentCounter, input2);
+                //Console.WriteLine($"PreviousNumber = {previousNumber} => newNumber = {newNumber}");
+                if (input2.ContainsKey(previousNumber))
+                {
+                    input2[previousNumber] = currentCounter - 1;
+                }
+                else
+                {
+                    input2.Add(previousNumber, currentCounter - 1);
+                }
+                previousNumber = newNumber;
+                currentCounter++;
+            }
+            //input.Keys.ToList().ForEach(x => Console.WriteLine($"Number: {x}, last seen: {input[x]}"));
+            Console.WriteLine($"The number on position {goal} = {previousNumber}");
         }
 
         public override void GatherInput()
