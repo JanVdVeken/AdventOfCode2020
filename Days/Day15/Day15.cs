e70e818dce038c15d5345b06273a61b9d949587e
PROTOPIA\jan.vanderveken <jan.vanderveken@protime.eu>
2020-12-15 12:24:47 +0100

    Solved D15P1

--- Days/Day15/Day15.cs
+++ Days/Day15/Day15.cs
@@ -9,17 +9,37 @@ namespace AdventOfCode2020.Days
 {
     public class Day15 : Day
     {
-        private List<string> input = new List<string>();
+        private Dictionary<int,int> input = new Dictionary<int, int>();
 
         public Day15()
         {
             DayNumber = 15;
-            Title = "TBD";
+            Title = "Rambunctious Recitation";
         }
 
         public override void Puzzle1()
         {
-
+            //input.Keys.ToList().ForEach(x => Console.WriteLine($"Number: {x}, last seen: {input[x]}"));
+            int currentCounter = input.Count()+1;
+            int previousNumber = input.Keys.Last();
+            input.Remove(input.Keys.Last());
+            while (currentCounter <= 2020)
+            {
+                int newNumber = Memory.GetNewNumber(previousNumber, currentCounter, input);
+                //Console.WriteLine($"PreviousNumber = {previousNumber} => newNumber = {newNumber}");
+                if(input.ContainsKey(previousNumber))
+                {
+                    input[previousNumber] = currentCounter-1;
+                }
+                else
+                {
+                    input.Add(previousNumber, currentCounter-1);
+                }
+                previousNumber = newNumber;
+                currentCounter++;
+            }
+            //input.Keys.ToList().ForEach(x => Console.WriteLine($"Number: {x}, last seen: {input[x]}"));
+            Console.WriteLine($"The number on position 2020 = {previousNumber}");
         }
 
         public override void Puzzle2()
@@ -30,7 +50,8 @@ namespace AdventOfCode2020.Days
 
         public override void GatherInput()
         {
-            input = ReadFile().ToList();
+            var tempinput = ReadFile().First().Split(',').Select(int.Parse).ToList();
+            input = tempinput.ToDictionary(x => x, x => tempinput.IndexOf(x) + 1);
         }
     }
 }
---
+++ Days/Day15/Memory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Days.Day15
+{
+    class Memory
+    {
+        public static int GetNewNumber(int previousNumber, int currentCounter, Dictionary<int,int> input)
+        {
+            if(input.ContainsKey(previousNumber))
+            {
+                return currentCounter - input[previousNumber] -1 ;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
