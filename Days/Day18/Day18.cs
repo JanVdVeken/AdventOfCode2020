188882a1145eca972e91f81f28ce63ecb1812ad2
JanVdV <vekie1@hotmail.com>
2020-12-17 19:37:06 +0100

    Ready for Day18

---
+++ Days/Day18/Day18.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCode2020.Shared;
+using Days.Day18;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020.Days
+{
+    public class Day18 : Day
+    {
+        private List<string> input = new List<string>();
+
+        public Day18()
+        {
+            DayNumber = 18;
+            Title = "TBD";
+        }
+
+        public override void Puzzle1()
+        {
+
+        }
+
+        public override void Puzzle2()
+        {
+
+
+        }
+
+        public override void GatherInput()
+        {
+            input = ReadFile().ToList();
+        }
+    }
+}
