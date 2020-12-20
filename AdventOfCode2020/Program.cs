c82493e207cce654128c86372d261516f55164ac
JanVdV <vekie1@hotmail.com>
2020-12-20 19:10:57 +0100

    Ready for day20

--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -29,6 +29,7 @@ namespace AdventOfCode2020
             new Day17(),
             new Day18(),
             new Day19(),
+            new Day20(),
         };
         static void Main(string[] args)
         {
---
+++ Days/Day20/Day20.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCode2020.Shared;
+using Days.Day20;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020.Days
+{
+    public class Day20 : Day
+    {
+        private List<string> input = new List<string>();
+
+        public Day20()
+        {
+            DayNumber = 20;
+            Title = "Jurassic Jigsaw";
+        }
+
+        public override void Puzzle1()
+        {
+
+        }
+
+        public override void Puzzle2()
+        {
+
+
+        }
+
+        public override void GatherInput()
+        {
+            input = ReadFile().ToList();
+        }
+    }
+}
