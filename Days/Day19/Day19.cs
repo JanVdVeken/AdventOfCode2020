114686e8f32fb8341d61bdbebecc699cefa10b2e
JanVdV <vekie1@hotmail.com>
2020-12-20 18:15:16 +0100

    Solved Day19P1

--- Days/Day19/Day19.cs
+++ Days/Day19/Day19.cs
@@ -4,12 +4,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2020.Days
 {
     public class Day19 : Day
     {
         private List<string> input = new List<string>();
+        private List<string> regexes = new List<string>();
+        private List<string> messages = new List<string>();
 
         public Day19()
         {
@@ -19,6 +22,18 @@ namespace AdventOfCode2020.Days
 
         public override void Puzzle1()
         {
+            Dictionary<int, string> dictRegexes = MessageHelper.CreateRegexDict(regexes);
+            Regex regex0 = new Regex("^"+dictRegexes[0]+"$");
+            var ans = 0;
+            foreach(string message in messages)
+            {
+                if (regex0.IsMatch(message))
+                {
+                    //Console.WriteLine(message);
+                    ans++;
+                }
+            }
+            Console.WriteLine($"We have found {ans} matches");
 
         }
 
@@ -31,6 +46,14 @@ namespace AdventOfCode2020.Days
         public override void GatherInput()
         {
             input = ReadFile().ToList();
+            foreach (string line in input.Where(x=>input.IndexOf(x) < input.IndexOf(string.Empty)))
+            {
+                regexes.Add(line.Replace("\"",""));
+            }
+            foreach (string line in input.Where(x => input.IndexOf(x) > input.IndexOf(string.Empty)))
+            {
+                messages.Add(line);
+            }
         }
     }
 }
---
+++ Days/Day19/MessageHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Days.Day19
+{
+    class MessageHelper
+    {
+        public static Dictionary<int, string> CreateRegexDict(List<string> regexes)
+        {
+            Dictionary<int, string> regexesDict= new Dictionary<int, string>();
+            foreach(string line in regexes)
+            {
+                regexesDict.Add(int.Parse(line.Split(": ")[0]), line.Split(": ")[1]);
+            }
+            //regexesDict.Keys.ToList().ForEach(x => Console.WriteLine($"{x} => {regexesDict[x]}"));
+            //Console.WriteLine(solveDict(regexesDict, 0));
+            regexesDict[0] = solveDict(regexesDict,0);
+            return regexesDict;
+        }
+
+        private static string solveDict(Dictionary<int, string> dictToSolve, int toSolve)
+        {
+            StringBuilder sb = new StringBuilder();
+            if(dictToSolve[toSolve].Contains(" | "))
+            {
+                sb.Append("(");
+            }
+            foreach (string part in dictToSolve[toSolve].Split(" "))
+            {
+                if (int.TryParse(part, out int parsedPart))
+                {
+                    sb.Append(solveDict(dictToSolve, parsedPart));    
+                }
+                else
+                {
+                    sb.Append(part);
+                }
+            }
+            if (dictToSolve[toSolve].Contains(" | "))
+            {
+                sb.Append(")");
+            }
+            return sb.ToString();
+        }
+    }
+}
