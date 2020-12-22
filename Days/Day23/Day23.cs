030f91a33acfa35e39aea2c0a4b113296971dfac
JanVdV <vekie1@hotmail.com>
2020-12-22 23:36:10 +0100

    Ready For Day23

---
+++ Days/Day23/Day23.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCode2020.Shared;
+using Days.Day23;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020.Days
+{
+    public class Day23 : Day
+    {
+        private List<string> input = new List<string>();
+
+        public Day23()
+        {
+            DayNumber = 23;
+            Title = "TBD";
+        }
+
+        public override void Puzzle1()
+        {
+
+        }
+
+        public override void Puzzle2()
+        {
+
+
+        }
+
+        public override void GatherInput()
+        {
+            input = ReadFile().ToList();
+        }
+    }
+}
